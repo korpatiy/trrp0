a7e40ea6d415c25a00eb8f2fb07159d036d18cc4
Vyacheslav Tretyakov <tretiakovslava@gmail.com>
2021-09-08 15:30:20 +0500

    Initial commit

---
+++ src/main/kotlin/com/trrp/trrp0/Trrp0Application.kt
@@ -0,0 +1,11 @@
+package com.trrp.trrp0
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Trrp0Application
+
+fun main(args: Array<String>) {
+	runApplication<Trrp0Application>(*args)
+}
---
+++ src/test/kotlin/com/trrp/trrp0/Trrp0ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.trrp.trrp0
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Trrp0ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
